26b467721ecc6e55d118ffe461176d01bd4ea401
Chris Wald <cwald@epic.com>
2021-09-01 09:42:20 -0500

    Initial commit

---
+++ PodcastMetadataDownload/Program.cs
@@ -0,0 +1,68 @@
+﻿//----------------------------------------------------
+// Copyright 2021 Epic Systems Corporation
+//----------------------------------------------------
+
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Podcast
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			StringBuilder sb = new StringBuilder();
+			int start = 1;
+			int count = RemainingCount(start);
+
+			while (count > 0)
+			{
+				string html = GetNextHtml(start);
+				sb.AppendLine(html);
+				start += count;
+
+				count = RemainingCount(start);
+			}
+
+			File.WriteAllText("results.html", sb.ToString());
+		}
+
+		static int RemainingCount(int start)
+		{
+			string resultString = string.Empty;
+			string url = $"https://www.npr.org/get/510208/remainingCount?start={start}";
+
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+			request.AutomaticDecompression = DecompressionMethods.GZip;
+
+			using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+			using (Stream stream = response.GetResponseStream())
+			using (StreamReader reader = new StreamReader(stream))
+			{
+				resultString = reader.ReadToEnd();
+			}
+
+			return int.Parse(resultString);
+		}
+
+		static string GetNextHtml(int start)
+		{
+			string resultString = string.Empty;
+			string url = $"https://www.npr.org/get/510208/render/partial/next?start={start}";
+
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+			request.AutomaticDecompression = DecompressionMethods.GZip;
+
+			using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+			using (Stream stream = response.GetResponseStream())
+			using (StreamReader reader = new StreamReader(stream))
+			{
+				resultString = reader.ReadToEnd();
+			}
+
+			return resultString;
+		}
+	}
+}
---
+++ PodcastMetadataParse/Program.cs
@@ -0,0 +1,95 @@
+﻿//----------------------------------------------------
+// Copyright 2021 Epic Systems Corporation
+//----------------------------------------------------
+
+using System;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace PodcastMetadataParse
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length != 1 ||
+				!File.Exists(args[0]))
+			{
+				Console.WriteLine("First parameter must be file that exists");
+				return;
+			}
+
+			XmlDocument doc = new XmlDocument();
+			doc.Load(args[0]);
+
+			XmlNode root = doc.FirstChild;
+
+			foreach (XmlNode div in root.ChildNodes)
+			{
+				foreach (XmlNode article in div.SelectNodes("article"))
+				{
+					ProcessArticle(article);
+				}
+			}
+		}
+
+		static void ProcessArticle(XmlNode article)
+		{
+			Regex regex = new Regex(@"\s{2,}", RegexOptions.Multiline);
+
+			XmlNode itemInfo = article.FirstChild;
+
+			// Get the publish date
+			XmlNode timeNode = itemInfo.SelectSingleNode("h3/time");
+			string time = timeNode.Attributes.GetNamedItem("datetime").Value;
+
+			if (Directory.Exists(time))
+			{
+				Directory.Delete(time, recursive: true);
+			}
+
+			Directory.CreateDirectory(time);
+
+			File.WriteAllText(Path.Combine(time, "date.txt"), time);
+
+			// Get the episode title
+			XmlNode titleNode = itemInfo.SelectSingleNode("h2/a");
+			string title = titleNode.InnerText;
+			title = regex.Replace(title, " ");
+			File.WriteAllText(Path.Combine(time, "title.txt"), title);
+
+			// Get the teaser
+			XmlNode teaserNode = itemInfo.SelectSingleNode("p[@class=\"teaser\"]");
+			string teaser = teaserNode.InnerText;
+			teaser = regex.Replace(teaser, " ");
+			File.WriteAllText(Path.Combine(time, "teaser.txt"), teaser);
+
+			// Get the episode URL
+			XmlNode episodeNode = itemInfo.SelectSingleNode("article/div/div/div/a[@class=\"audio-module-listen\"]");
+			if (episodeNode != null)
+			{
+				string url = episodeNode.Attributes.GetNamedItem("href").Value;
+				File.WriteAllText(Path.Combine(time, "url.txt"), url);
+				DownloadFile(url, time);
+			}
+		}
+
+		static void DownloadFile(string url, string outDir)
+		{
+			using (WebClient webClient = new WebClient())
+			{
+				Uri uri = new Uri(url);
+				string filename = Path.GetFileName(uri.LocalPath);
+
+				if (filename.Contains("?"))
+				{
+					filename = filename.Substring(0, filename.IndexOf('?'));
+				}
+
+				webClient.DownloadFile(url, Path.Combine(outDir, filename));
+			}
+		}
+	}
+}
